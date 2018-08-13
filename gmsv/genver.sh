698c9f9eb4695bea11cf3e7bb917c654bc40cc3e
Gavin Lin <gavin.lin.asd@gmail.com>
2018-08-13 00:41:54 -0700

    Fix genver.sh so that it will generate the multi-line string literal correctly.

--- gmsv/genver.sh
+++ gmsv/genver.sh
@@ -2,8 +2,8 @@
 IN=./include/version.h
 OUT=./genver.h
 touch main.c
-echo "char *genver=\"" > $OUT
+echo "char *genver=" > $OUT
 cat $IN | grep -e "^#define" | cut -b 9- | sed "s/\"/\'/g" | \
-  sed 's/\\/\\\\/g' >> $OUT
-echo "<<Generated at "`date`">>" >> $OUT
-echo "by : 龙zoro工作室\n\";" >> $OUT
+  sed 's/\\/\\\\/g' | sed 's/^/\"/g' | sed 's/$/\"/g' >> $OUT
+echo "\"<<Generated at "`date`">>\"" >> $OUT
+echo "\"by : 龙zoro工作室\";" >> $OUT
